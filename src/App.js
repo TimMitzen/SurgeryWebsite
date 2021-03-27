e12891c9b3ef3db3cceaf2fbea9f35cec1ed787c
timislearning <tim.mitzen@gmail.com>
2021-03-27 10:07:53 -0400

    Added some Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import { Router } from 'react-router-dom';
+import Routes from './routes/Routes'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Router>
+          <Routes/>
+        </Router>
       Surgery Website
        
       </header>
---
+++ src/routes/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Home from '../components/Home'
+
+
+const Routes = () =>{
+   return(
+      <Switch>
+         <Route exact path='/Surgery' component ={Home}/>
+      </Switch>
+   );
+};
+
+export default Routes;
