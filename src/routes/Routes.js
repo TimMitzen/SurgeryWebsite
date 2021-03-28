cdfaadaf2c8ba7a7c122f9e5babc76c787364d39
timislearning <tim.mitzen@gmail.com>
2021-03-28 13:01:33 -0400

    add Drives, Imaging and Ricoh Routes'

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from '../components/Home'
-
+import Drives from '../components/Drives'
+import Ricoh from '../components/Ricoh'
+import Imaging from '../components/Imaging'
 
 const Routes = () =>{
    return(
       <Switch>
          <Route exact path='/Surgery' component ={Home}/>
+         <Route path='/Drives' component={Drives}/>
+         <Route path ='/Imaging' component={Imaging}/>
+         <Route path ='/Ricoh' component={Ricoh}/>
       </Switch>
    );
 };
