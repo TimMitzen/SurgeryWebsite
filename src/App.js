9c094ac4ebf689fea98fa474d4af70788a60f05c
timislearning <tim.mitzen@gmail.com>
2021-03-29 12:43:43 -0400

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes/Routes'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './navbar/NavBar';
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+  integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+  crossorigin="anonymous"
+/>
+
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <Router>
           <Routes/>
         </Router>
+        
       Surgery Website
        
       </header>
