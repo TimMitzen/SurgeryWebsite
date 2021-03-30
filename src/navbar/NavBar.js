1ed4270515b7f0490745f8069e11dbc8b3ebd313
timislearning <tim.mitzen@gmail.com>
2021-03-30 12:02:30 -0400

    deploy

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -4,7 +4,7 @@ import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
 const NavBar =() =>{
    return(
 <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">Welcome</Navbar.Brand>
+  <Navbar.Brand className ='welcome' href="#home">Welcome</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
