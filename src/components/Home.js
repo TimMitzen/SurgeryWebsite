6565529b5fd694a0ba633f227b3c7f94fe53350c
timislearning <tim.mitzen@gmail.com>
2021-03-30 16:26:26 -0400

    deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+
 const Home = () => {
    return(
       <div>
          Surgery IS Website
-         <Link to='/imaging'>Imaging</Link>
+         
       </div>
    );
 }
