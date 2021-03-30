cd7f002678dcbf304db42c2c5249fba2d27ec687
timislearning <tim.mitzen@gmail.com>
2021-03-30 16:46:43 -0400

    deploy

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -1,30 +1,26 @@
 import React from 'react'
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
-import {Link} from 'react-router-dom'
-import Imaging from '../components/Imaging'
+
 
 const NavBar =() =>{
-   return(
-      <div>
-      <Link to = '/imaging' component ={Imaging}>Images</Link>
+  return(
 <Navbar bg="light" expand="lg">
-  <Navbar.Brand className ='welcome' href="#home">Welcome</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <NavDropdown.Item href="/SurgeryWebsite/">Home</NavDropdown.Item>
-      <Nav.Link href="https://surgery-inventory.herokuapp.com/">Inventory</Nav.Link>
+ <Navbar.Brand href="#home">Welcome</Navbar.Brand>
+ <Navbar.Toggle aria-controls="basic-navbar-nav" />
+ <Navbar.Collapse id="basic-navbar-nav">
+   <Nav className="mr-auto">
+     <Nav.Link href="/SurgeryWebsite">Home</Nav.Link>
+     <Nav.Link href="https://surgery-inventory.herokuapp.com/">Inventory</Nav.Link>
+     <NavDropdown title="Menu" id="basic-nav-dropdown">
+       <NavDropdown.Item href="/Imaging">Imaging Steps</NavDropdown.Item>
+       <NavDropdown.Item href="/Video">Video Conferencing</NavDropdown.Item>
+       <NavDropdown.Item href="/Drives">Drive Mappings</NavDropdown.Item>
       
-        <NavDropdown.Item href="/imaging" >Imaging Steps</NavDropdown.Item>
-        <NavDropdown.Item href="/video">Video Conferencing</NavDropdown.Item>
-        <NavDropdown.Item href="/drives">Drive Mappings</NavDropdown.Item>
-        <NavDropdown.Item href="/ricoh">Ricoh</NavDropdown.Item>
-      
-    </Nav>
-    
-  </Navbar.Collapse>
-</Navbar>
-</div>
-)
+       <NavDropdown.Item href="/Ricoh">Ricoh</NavDropdown.Item>
+     </NavDropdown>
+   </Nav>
+   
+ </Navbar.Collapse>
+</Navbar>)
 }
 export default NavBar;
