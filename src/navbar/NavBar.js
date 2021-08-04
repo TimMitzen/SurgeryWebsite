a790bd7d05064d975419243bf921b8e72f6928c1
Tim Mitzen <45426633+TimMitzen@users.noreply.github.com>
2021-08-04 13:10:20 -0400

    Update NavBar.js

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -11,7 +11,8 @@ const NavBar =() =>{
    <Nav className="mr-auto">
      <Nav.Link href="/SurgeryWebsite">Home</Nav.Link>
      <Nav.Link href="https://surgery-inventory.herokuapp.com/">Inventory</Nav.Link>
-     <NavDropdown title="Menu" id="basic-nav-dropdown">
+    <Nav.Link href="timisone.pythonanywhere.com">Non-Ricoh Printers</Nav.Link>
+    <NavDropdown title="Menu" id="basic-nav-dropdown">
        <NavDropdown.Item href="/Imaging">Imaging Steps</NavDropdown.Item>
        <NavDropdown.Item href="/Video">Video Conferencing</NavDropdown.Item>
        <NavDropdown.Item href="/Drives">Drive Mappings</NavDropdown.Item>
